50d539059d62df7e824d9e08d29a446ddfb5cc51
Vansh Kamboj <vanshkamboj2303@gmail.com>
2026-08-20 18:00:19 +0530

    deployment issue fix

--- src/main.jsx
+++ src/main.jsx
@@ -2,28 +2,39 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./routes/App.jsx";
-import {createBrowserRouter, RouterProvider} from "react-router-dom"
-import Bag from "./routes/Bag.jsx"
-import Home from "./routes/Home"
-import {Provider} from "react-redux"
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Bag from "./routes/Bag.jsx";
+import Home from "./routes/Home";
+import { Provider } from "react-redux";
 import myntraStore from "./store/index.js";
-import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/css/bootstrap.min.css";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/bag",
+          element: <Bag />,
+        },
+      ],
+    },
+  ],
   {
-    path: "/",
-    element: <App />,
-    children: [
-      { path: "/", element: <Home/>, },
-      { path: "/bag", element: <Bag/>,/*action: createPostAction */},
-    ],
-  },
-]);
+    basename: "/myntra_project",
+  }
+);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={myntraStore}>
-      <RouterProvider router={router}/>
+      <RouterProvider router={router} />
     </Provider>
   </StrictMode>
 );
